d6cc67d5a7a0e9d2e21da7adc839d267d41f9d00
Ivan Jester <jesterivan4@gmail.com>
2025-01-23 16:33:25 +0800

    Added form layout dynamically

---
+++ resources/js/Components/TextLink.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    routeName: String,
+    label: String,
+})
+</script>
+
+<template>
+    <!--Now use Link component which is part of Innertia-->
+    <Link :href="route(routeName)" class="text-indigo-500 hover:text-indigo-600 font-medium underline dark:text-indigo-400 dark:hover:text-indigo-500">{{ label }}</Link>
+</template>
+
+<!--Our Text Link component is Expecting a routeName and a label-->
+<!--Now import the Link component to register.vue-->
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,15 +1,26 @@
 <script setup>
 import Container from '../../Components/Container.vue';
 import Title from '../../Components/Title.vue';
+import TextLink from '../../Components/TextLink.vue';
+import InputField from '../../Components/InputField.vue';
 </script>
 
 
 <template>
     <!--Use our Container-->
-    <Container>
+    <Container class="w-1/2">
         <div class="mb-8 text-center">
             <Title>Register a new account</Title>
-            <p>Already have an account? Login</p> <!--and also login as link-->
+            <p>Already have an account?
+                <TextLink routeName="home" label="Login"/>
+            </p> <!--and also login as link-->
         </div>
+
+        <form class="space-y-6">
+            <InputField label="Name" icon="id-badge"/>
+            <InputField label="Email" type="email" icon="at"/>
+            <InputField label="Password" type="password" icon="key"/>
+            <InputField label="Name" type="password" icon="at"/>
+        </form>
     </Container>
 </template>
