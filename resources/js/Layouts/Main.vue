54d0b9d9d1c1aa36c4368b381b223911069a9a8a
Ivan Jester <jesterivan4@gmail.com>
2025-01-24 08:44:44 +0800

    Adding some Components
    Dynamic

--- resources/js/Layouts/Main.vue
+++ resources/js/Layouts/Main.vue
@@ -11,13 +11,17 @@ import NavLink from '../Components/NavLink.vue'; // import NavLink
             <NavLink routeName="home" componentName="Home">Home</NavLink> <!-- this is inertia link-->
 
             <div class="flex items-center space-x-6">
-            <NavLink routeName="register" componentName="Auth/Register">Register</NavLink>
+
+                <NavLink routeName="register" componentName="Auth/Register">
+                    Register
+                </NavLink>
                 <button
                     @click="switchTheme()" 
                     class=" hover:bg-slate-700 w-6 h-6 grid place-items-center rounded-full hover:outline outline-1 outline-white" >
                     
                     <i class="fa-solid fa-circle-half-stroke"></i>
                 </button>
+                
             </div>
         </nav>
     </header>
